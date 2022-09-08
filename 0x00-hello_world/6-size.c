bec2775a8a87e2106d3d38b8617799fee6fb1f93
Tolulope Gbenga Olaoluwa <Tophepzz14@gmail.com>
2022-09-08 14:51:36 -0700

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-int main(void)
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
